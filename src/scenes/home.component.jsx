2ea5e5ab1b65372d033aef9776be4ff86b70fa46
Patryk Gruchot <patryk.gruchot@outlook.com>
2023-07-09 14:49:20 +0200

    added updated logo component

--- src/scenes/home.component.jsx
+++ src/scenes/home.component.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Background from "../components/background/background.component";
-
+import Logo from "../components/logo/logo.component";
 function Home() {
   return (
     <>
       <Background />
+      <Logo />
     </>
   );
 }
